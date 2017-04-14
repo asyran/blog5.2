614e46f5564dea38b761349c4b9c9665d3b76d81
asyran <wanasyran1991@gmail.com>
2017-04-14 17:19:05 +0800

    first commit

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+   
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+   
+
+ public function about()
+    {
+
+        return view('about');
+    }
+
+}
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome</div>
+
+                <div class="panel-body">
+                    Your Application's Landing Page.
+
+                    This is my page
+                    power rangger
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
